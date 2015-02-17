32d46d682361f84375fc7c4c54c631c443fe4e9f
yjacolin <yves@georezo.net>
2015-02-17 15:39:01 +0100

    [WIP] Improve dockerfile for georezo container. Missing feature: deploy git repository, install of drush, etc.

--- GeoRezo/start.sh
+++ GeoRezo/start.sh
@@ -2,7 +2,10 @@
 
 # http://juliendubreuil.fr/blog/drupal/installer-drush-pour-administrer-drupal/
 
-# mysqladmin -p create georezo
+service mysql start
+service apache2 start
+# wrong command, we don't know root password :/
+mysqladmin -p create georezo 
+drush make grz_portal.make
 drush si georezo --db-url=mysql://georezo:georezo@localhost/georezo --db-su=root --db-su-pw=georezo
-#drush make grz_portal.make
 
