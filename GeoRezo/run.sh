82fb3c6cd4125cefbf94f8405ea619d4cbd2f009
yjacolin <yves@georezo.net>
2015-03-03 21:29:39 +0100

    update Docker install for GeoRezo (drupal)

--- GeoRezo/run.sh
+++ GeoRezo/run.sh
@@ -3,5 +3,6 @@ docker run --name="georezo" \
 -p 5000:80 \
 -v ${HOME}/.ssh:/root/.ssh:ro \
 -v ${HOME}:/home/${USER} \
+-v ${HOME}/Documents/Developpement/Yjacolin/georezov3:/home/georezo/ \
 yjacolin/georezo:drupal
 bash
--- GeoRezo/start.sh
+++ GeoRezo/start.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+echo "Getting drush command"
+curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer && ln -s /usr/local/bin/composer /usr/bin/composer && sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc && source $HOME/.bashrc && composer global require drush/drush:dev-master 
+
 # http://juliendubreuil.fr/blog/drupal/installer-drush-pour-administrer-drupal/
 
+echo "Starting apache and MySQL"
 service mysql start
 service apache2 start
-# wrong command, we don't know root password :/
-mysqladmin -p create georezo 
-drush make grz_portal.make
-drush si georezo --db-url=mysql://georezo:georezo@localhost/georezo --db-su=root --db-su-pw=georezo
 
+echo "Setting root MySQL DB password"
+mysqladmin -u root password georezo
+echo "Creating MySQL DB"
+mysqladmin --password=georezo create georezo 
+echo "Creating MySQL DB user"
+mysql --password=georezo -e "CREATE USER 'georezo'@'localhost' IDENTIFIED BY 'georezo';"
+mysql --password=georezo -e "GRANT ALL PRIVILEGES ON * . * TO 'georezo'@'localhost';"
+mysqladmin --password=georezo reload
+
+echo "First run of GeoRezo make command"
+cd /home/georezo/grz_portal/
+drush make grz_portal.make
+drush si georezo_profile --db-url=mysql://georezo:georezo@localhost/georezo --db-su=root --db-su-pw=georezo --site-name=GeoRezo --site-mail=noreply@georezo.net --account-name=editeur --account-mail=editeur@georezo.net --account-pass=editeur
