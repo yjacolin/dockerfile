e0d7a06a0894e44e5ea5f9e4277fd94433da5c3b
Yves Jacolin <yjacolin@free.fr>
2015-03-07 18:46:24 +0100

    Update build for QME

--- QMapfileEditor/run-qme-in-docker.sh
+++ QMapfileEditor/run-qme-in-docker.sh
@@ -5,12 +5,13 @@ xhost +
 # --rm will remove the container as soon as it ends
 # -v ${HOME}/.config:/.config \
 # -v ${HOME}/.gtkrc-2.0:/.gtkrc-2.0 \
-docker run --rm --name="qmapfileeditor" \
+docker run --rm --name="qmapfileeditor-tmp" \
 -i -t \
 -v ${HOME}/.gtkrc-2.0:/.gtkrc-2.0 \
 -v ${HOME}:/home/${USER} \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -e DISPLAY=unix$DISPLAY \
-yjacolin/qmapfileeditor:v0.1 \
-/home/QMapfileEditor/QMapfileEditor
+yjacolin/qmapfileeditor:latest \
+bash
+# /home/yves/Documents/Developpement/Yjacolin/QMapfileEditor/QMapfileEditor
 xhost -
